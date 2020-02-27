43302a86f792763d6a35e7217a688b3b9c09079d
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-02-27 10:43:16 -0700

    Update Main.cpp
    Adder a header comment

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,7 @@
+/*This is the main test program for the Github banking project
+Created by the ABBA team
+Period 3 Computer Science
+Febuary 27th 2020*/
 #include "Savings.h"
 #include "HELOC.h"
 #include "Checking.h"
