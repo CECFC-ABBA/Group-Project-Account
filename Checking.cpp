af01b15adfb3a63c42b0fb2494b49e44129d3fac
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-02-28 10:28:32 -0700

    I created defaults for the constructor and destructor along with code for how the fee affects the account's balance

--- Checking.cpp
+++ Checking.cpp
@@ -1,14 +1,27 @@
 #include "Checking.h"
+#include "Account.h"
+#include <iostream>
 
-Checking::Checking() {
+/*Constructor for Checking*/
+Checking::Checking(intrest, feeCost) {
+	intrest = intr;
+	feeCost = fee;
+};
 
-}
-
+/*Destructor for Checking*/
 Checking::~Checking() {
-
+	delete []intrest;
+	delete[]feeCost;
 }
 
+/*Code foe determining if the account has the money to handle a fee and reducing the balance if so*/
 string Checking::Transaction(double amt) {
-
+	amt = bal;
+	if (feeCost > bal) {
+		std::cout << "There is not enougn money in your account" << endl;
+	}
+	else {
+		std::cout << "Your account has a sufficent balance" << endl;
+		bal = bal - feeCost;
+	}
 }
-//Brennan
