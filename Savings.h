6755bb8c215d4938e739329e5c405dec0bab7efd
agallegos-13 <61513494+agallegos-13@users.noreply.github.com>
2020-02-28 10:55:03 -0700

    Update Savings.h

--- Savings.h
+++ Savings.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "Account.h"
 typedef class Savings final : public Account {
+private:
+	string accountname;
+	double bal, interest;
+	int withdraws = 0, months, maxWithdraw;
+	friend class Account;
 public:
 	Savings();
 	~Savings();
+	void Print();
 	string Transaction(double) final;
 } Checking;
 //Anela
