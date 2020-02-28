c0469d0cf375337953a8c1f20726244fd4c4e9a0
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-02-28 10:25:58 -0700

    Update Checking.h
    I added public variables for Checking.cpp to reference

--- Checking.h
+++ Checking.h
@@ -4,8 +4,9 @@ typedef class Checking final : public Account {
 	double intr = 0;
 	double fee = 0;
 public:
+	double intrest = intr;
+	double feeCost = fee;
 	Checking();
 	~Checking();
 	string Transaction(double) final;
 } Checking;
-//Brennan
