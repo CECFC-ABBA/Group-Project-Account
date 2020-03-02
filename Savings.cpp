ea1adda75c491e559079beac0cc86a89a30ab5df
agallegos-13 <61513494+agallegos-13@users.noreply.github.com>
2020-03-02 10:40:13 -0700

    Update Savings.cpp

--- Savings.cpp
+++ Savings.cpp
@@ -2,11 +2,12 @@
 Savings Class Implementations
 2/28/20*/
 #include "Savings.h"
+#include "string.h"
 
 Savings::Savings() {
-	bal = Account:getBal();
+	bal = getBal();
 	//months = ??
-	interest = 1.75;
+	interest = getIntr();
 	maxWithdraw = 5; //temp val
 }
 
@@ -20,21 +21,22 @@ void Savings::Print() {
 		if (i == 12) years++;
 	}
 	cout << "The current balance of " << accountname << " is: " << bal << endl;
-	cout << accountname << " has been active for " << ((years > 0) " and ")) << ((months > 0) months)) << "." << endl;
+	cout << accountname << " has been active for " << years << " years and " << months << " months." << endl;
 	cout << "Interest Rate: " << interest << "%\n";
 }
 
 string Savings::Transaction(double amt) {
 	double withdrawl;
+	bool con = false;
 	char e;
 	//assuming transaction is only called on if withdrawl is being made
 	do {
 		cout << "How much would you like to withdraw? ";
 		cin >> withdrawl;
 		if (withdrawl > bal) cout << "Insuffient funds. Enter another value or E to exit." << endl;
-	} while (!e == 'E' || !e == 'e');
+		if (!e == 'E' || !e == 'e') con = true;
+	} while (con == false);
 	bal -= withdrawl;
-	amt = bal;
-	return amt;
+	string trans = to_string(bal);
 }
 //Anela
