139c75b01f9aa1886fc1b30ecfb17ff418092573
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-02 10:43:59 -0700

    Did Troubleshooting

--- Checking.h
+++ Checking.h
@@ -3,10 +3,18 @@
 typedef class Checking final : public Account {
 	double intr = 0;
 	double fee = 0;
+	double bal = 0;
+	double overDraft;
 public:
 	double intrest = intr;
 	double feeCost = fee;
+	double balance = bal;
 	Checking();
 	~Checking();
 	string Transaction(double) final;
+	double getOverDraft() const {
+		return overDraft;
+	}
 } Checking;
+#define MAIN
+//#define FANCYMAIN
