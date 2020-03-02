a8dfa8c18947841bb38efb744243a33c5e7f2158
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-02 10:44:29 -0700

    Did Troubleshooting

--- Checking.cpp
+++ Checking.cpp
@@ -1,33 +1,34 @@
 #include "Checking.h"
 #include "Account.h"
-#include <iostream>
+#include <string>
 
 /*Constructor for Checking*/
 Checking::Checking(intrest, feeCost) {
-	intrest = intr;
-	feeCost = fee;
+	intr = intrest;
+	fee = feeCost;
 };
 
 /*Destructor for Checking*/
 Checking::~Checking() {
-	delete []intrest;
+	delete[]intrest;
 	delete[]feeCost;
 }
 
 /*Code foe determining if the account has the money to handle a fee and reducing the balance if so*/
-string Checking::Transaction(double amt) {
-	amt = bal;
-	double overdraft;
-	string checkingOutput
-	if (feeCost > amt) {
-		overdraft = bal - feeCost
+class Checking::*Transaction(double amt) {
+	amt = balance;
+	double overdraft, excess, overdraftCost = 35.00;
+	string checkingOutput;
+	if (fee > amt) {
+		excess = bal - fee;
 		amt = 0;
 		checkingOutput = "There is not enougn money in your account. An overdraft will occur.";
+		overdraft = excess + overdraftCost;
 	}
 	else {
-		amt = amt - feeCost;
-		overdraft = 0;
+		amt = amt - fee;
+		excess = 0;
 		checkingOutput = "Your account has a sufficent balance.";
 	}
 	return checkingOutput;
-}
+};
