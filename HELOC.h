caa907a56d12e9d6f92ec6b103be6a09572e637a
Dirtsdevilbm <57111769+Dirtsdevilbm@users.noreply.github.com>
2020-03-03 10:16:38 -0700

    Update HELOC.h

--- HELOC.h
+++ HELOC.h
@@ -3,7 +3,7 @@
  * ABBA group memeber: Brendan McCleery.
  * 27 February 2020
  *	Purpose:
- * This is the Heloc class Inherrited from Account. 
+ * This is the HELOC class Inherrited from Account. 
  * This will keep track of home equity, the loan(and interest rate), and the fee being payed. 
  * (No fee, Charges, Interest)
  *
@@ -12,19 +12,40 @@
 #include "Account.h"
 
 /*  A HELOC is a form of loan that works more like a credit card. You are enabled to pull money from the equity of your home to be 
- * enabled to spend it. It'll have to be payed off in the monthly amounts based off of interest. 
+ * enabled to spend it. It'll have to be payed off in a monthly amounts based off of interest. 
  *
  */
 
 typedef class HELOC final : public Account {
-	//Equity 
 	double fee = 0;
 	double intr = 0;
 
+private:
+	//Equity
+	double equ;
+	
 public:
 	HELOC();
 	~HELOC();
-	string Transaction(double) final;
+	double getEqu() const {
+		return equ;
+
+	}
+
+	double getIntr() const {
+		return intr;
+
+	}
+
+	double getFee() const {
+		return fee;
+
+	}
+
+	void setIntr(double);
+	void setFee(double);
+	void setEqu(double);
+	virtual string Transaction(double);
 
 } HELOC;
 
