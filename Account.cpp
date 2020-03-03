d51d3d0b05d1ab4b870dfb26019eaaa208c5747d
AMarsh9879654321 <61513250+AMarsh9879654321@users.noreply.github.com>
2020-03-03 09:56:44 -0700

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,14 +1,13 @@
 #include "Account.h"
 
 void Account::setBal(double new_bal) {
-
+  bal = new_bal;
 }
 
 void Account::setIntr(double new_intr) {
-
+  intr = new_intr;
 }
 
 void Account::setFee(double new_fee) {
-
+  fee = new_fee;
 }
-//Alexander
