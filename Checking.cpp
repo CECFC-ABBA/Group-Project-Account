e371ef696c7e60c9c644a3cbea440fad505fc7f6
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-03 10:43:01 -0700

    Update Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -4,23 +4,25 @@
 
 /*Constructor for Checking*/
 Checking::Checking(intrest, feeCost) {
-	intr = intrest;
-	fee = feeCost;
+	double intr1 = intrest;
+	double fee1 = feeCost;
+	double bal1 = balance;
 };
 
 /*Destructor for Checking*/
 Checking::~Checking() {
 	delete[]intrest;
 	delete[]feeCost;
+	delete[]balance;
 }
 
-/*Code foe determining if the account has the money to handle a fee and reducing the balance if so*/
-class Checking::*Transaction(double amt) {
-	amt = balance;
+/*Code for determining if the account has the money to handle a fee and reducing the balance if so*/
+string Checking::Transaction(double amt) {
+	double amt = balance;
 	double overdraft, excess, overdraftCost = 35.00;
 	string checkingOutput;
 	if (fee > amt) {
-		excess = bal - fee;
+		excess = bal1 - fee;
 		amt = 0;
 		checkingOutput = "There is not enougn money in your account. An overdraft will occur.";
 		overdraft = excess + overdraftCost;
