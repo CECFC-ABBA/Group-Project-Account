afb67224ed2cde4d3940ba17b72ff3bf796f8c6d
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-04 10:42:02 -0700

    Update Checking.h

--- Checking.h
+++ Checking.h
@@ -1,3 +1,8 @@
+ /* This is the Checking class Inherrited from Account. 
+ * It will serve to create an overdraft for expenses which to not fit a budget 
+ * Period 3
+ * 4 March 2020
+ */
 #pragma once
 #include "Account.h"
 typedef class Checking final : public Account {/*Typedef is being ignored by the compiler*/
