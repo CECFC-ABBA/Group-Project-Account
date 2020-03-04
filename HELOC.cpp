5f4a2d89bf5971e4e3875b63f7e002f9167b0286
Dirtsdevilbm <57111769+Dirtsdevilbm@users.noreply.github.com>
2020-03-04 10:41:44 -0700

    Update HELOC.cpp

--- HELOC.cpp
+++ HELOC.cpp
@@ -1,6 +1,20 @@
+/*HELOC.cpp
+ * ABBA group memeber: Brendan McCleery.
+ * 3 March 2020
+ *	Purpose:
+ * Everything in the HELOC class will be defined here.
+ * This will keep track of home equity, the loan(and interest rate), and the fee being payed.
+ * (No fee(?), Charges, Interest)
+ *  The user will be enabled to see there current home equity. What kind of loan they can pull based off of that, with a random interest fee. 
+ * It will display how many months is needed to pay off that loan and your current equity after pulling the loan.
+ *
+ */
+
 #include "HELOC.h"
+#include <string>
 
 HELOC::HELOC() {
+    double equ = equity;
 
 }
 
@@ -11,4 +25,4 @@ HELOC::~HELOC() {
 string HELOC::Transaction(double amt) {
   return "";
 }
-//Brendan
+
