13669309a1ea7210c47a45532a82004116db40e8
AMarsh9879654321 <61513250+AMarsh9879654321@users.noreply.github.com>
2020-03-05 10:39:04 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char** argv) {
     vector<string> filedata;
     string cur_line;
     do {
+      cur_line = "";
       file >> cur_line;
       filedata.push_back(cur_line);
     } while(cur_line != "");
