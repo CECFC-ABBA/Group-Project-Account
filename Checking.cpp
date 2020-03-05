4e77f4bc1447ecb6e0fb92d00d36feec255ccfa6
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-05 10:36:49 -0700

    Update Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -4,7 +4,7 @@
 
 /*Constructor for Checking*/
 Checking::Checking() {
-	
+	bal = 0;
 };
 
 /*Destructor for Checking*/
@@ -19,18 +19,17 @@ string Checking::Transaction(double amt) {
 	bal+=amt;
 	double overdraft, excess, overdraftCost = 35.00;
 	string checkingOutput;
-	if (fee > amt) {
-		excess = bal - fee;
-		amt = 0;
-		checkingOutput = "There is not enougn money in your account. An overdraft will occur.";
-		overdraft = excess + overdraftCost;
-	    
-	}
-	else {
-		amt = amt - fee;
+	if (fee < amt) {
+		bal = bal - fee;
 		excess = 0;
 		checkingOutput = "Your account has a sufficent balance.";
 	}
-	//You don't handle overdraft anywhere
+	else {
+		excess = bal - fee;
+		amt = 0;
+		checkingOutput = "There is not enough money in your account. An overdraft will occur.";
+		overdraft = excess + overdraftCost;
+		cout << "You owe the bank " << overdraft << " dollars." << endl;
+	}
 	return checkingOutput;
 };
