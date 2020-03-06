d72de30992232a3b5404dfe1c4ef7c08e40ba4bf
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-03-06 10:04:06 -0700

    Update Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -17,16 +17,17 @@ Checking::~Checking() {
 
 /*Code for determining if the account has the money to handle a fee and reducing the balance if so*/
 string Checking::Transaction(double amt) {
-	bal+=amt;
+	//bal+=amt;
+	amt = bal;
 	double overdraft, excess, overdraftCost = 35.00;
 	string checkingOutput;
-	if (fee < amt || bal >= 0) {
-		bal = bal - fee;
+	if (fee < amt || amt >= 0) {
+		amt = amt - fee;
 		excess = 0;
 		checkingOutput = "Your account has a sufficent balance.";
 	}
 	else {
-		excess = bal - fee;
+		excess = amt - fee;
 		amt = 0;
 		checkingOutput = "There is not enough money in your account. An overdraft will occur.";
 		overdraft = excess + overdraftCost;
