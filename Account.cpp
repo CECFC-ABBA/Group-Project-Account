aad66c81038f3d8fb1d4c54aa61de097b811ae97
AMarsh9879654321 <61513250+AMarsh9879654321@users.noreply.github.com>
2020-03-11 10:18:36 -0600

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,29 +1,50 @@
+#include "Checking.h"
 #include "Account.h"
+#include <string>
 
-Account::Account() {
-  
+/*Constructor for Checking*/
+Checking::Checking() {
+	bal = 0;
+	fee = 0;
+};
+
+/*Destructor for Checking*/
+Checking::~Checking() {
+	//delete[]this;
+	/*delete[]feeCost;
+	delete[]balance;*/
 }
 
-Account::~Account() {
- //delete[]this;
+
+string fixDouble(string doubl) {
+	int index = static_cast<int>(doubl.length() - 1);
+	for (int i = 0; i < static_cast<int>(doubl.length()); i++) {
+		if (doubl[static_cast<int>(doubl.length() - 1)-i] == '0') {
+			index--;
+		}
+		else {
+			break;
+		}
+	}
+	doubl = doubl.substr(0, index + 1);
+	return doubl;
 }
 
-void Account::setBal(double new_bal) {
-  bal = new_bal;
-}
-
-void Account::setIntr(double new_intr) {
-  intr = new_intr;
-}
-
-void Account::setFee(double new_fee) {
-  fee = new_fee;
-}
-
-void Account::setMonths(double new_months) {
-    months = new_months;
-}
-
-string Account::Transaction(double) {
-    return ""; 
-}
+/*Code for determining if the account has the money to handle a fee and reducing the balance if so*/
+string Checking::Transaction(double amt) {
+	bal+=amt;
+	double overdraft, excess, overdraftCost = 35.00;
+	string checkingOutput;
+	if (fee < amt) {
+		bal = bal - fee;
+		excess = 0;
+		checkingOutput = "Your account has a sufficent balance. You have $" + fixDouble(to_string(bal)) + " remaining after the $" + fixDouble(to_string(fee)) + " cost.\n";
+	}
+	else {
+		excess = bal - fee;
+		amt = 0;
+		overdraft = (excess - overdraftCost) * -1;
+		checkingOutput = "There is not enough money in your account. An overdraft will occur.\nYour account has been reduced to $0 and you owe the bank $" + fixDouble(to_string(overdraft)) + ".\n";
+	}
+	return checkingOutput;
+};
