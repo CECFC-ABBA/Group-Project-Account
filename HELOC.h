7363eff24c1d19855be190d648af72cc74b8a1ea
Dirtsdevilbm <57111769+Dirtsdevilbm@users.noreply.github.com>
2020-03-12 10:38:40 -0600

    Update HELOC.h

--- HELOC.h
+++ HELOC.h
@@ -1,5 +1,5 @@
 #pragma once
-/*HELOC.h
+/*HELOC.h :: Experimental
  * ABBA group memeber(s): Brendan McCleery, Anela Gallegos
  * 27 February 2020
  *	Purpose:
@@ -9,10 +9,11 @@
  *	4 March 2020
  * Messed with removing and adding "variables."
  *
- *
  */
 
 #include "Account.h"
+#include <iostream>
+using namespace std;
 
  /*  A HELOC is a form of loan that works more like a credit card. You are enabled to pull money from the equity of your home to be
   * enabled to spend it. It'll have to be payed off in a monthly amounts based off of interest. Over the time span of a few years dependant on the loan.
@@ -21,44 +22,50 @@
 
   //Equity, credit, interest, time till payed off at minimum pay. 
 
-	typedef class HELOC final : public Account {
+typedef class HELOC final : public Account {
 	double fee = 0;
-	double intr1 = 0, intr2 = 0, intr;
+	double intr1 = 0, intr2 = 0;
 
-	private:
-		double credit = 0;
-		double equity = 0;
-		double loan = 0;
-		double years = 0;
+private:
+	double credit = 0;
+	double equity = 0;
+	double loan = 0;
+	double years = 0;
+	double months = 0;
 
-	public:
-		HELOC();
-		~HELOC();
-		double getCredit() const {
-			return credit;
+public:
+	HELOC();
+	~HELOC();
+	double getCredit() const {
+		return credit;
 
-		}
+	}
 
-		double getEquity() const {
-			return equity;
+	double getEquity() const {
+		return equity;
 
-		}
+	}
 
-		double getLoan() const {
-			return loan;
+	double getLoan() const {
+		return loan;
 
-		}
+	}
+	double getPayment() const {
+		return payment;
 
-		double getYears() const {
-			return years;
+	}
 
-		}
+	double getYears() const {
+		return years;
 
-		void setEquity(double);
-		void setLoan(double);
-		void setYears(double);
-		void setCredit(double);
-		string fixDouble(string);
-		string Transaction(double) final;
+	}
+
+	void setCredit(double);
+	void setEquity(double);
+	void setLoan(double);
+	void setPayment(double);
+	void setYears(double);
+	string fixDouble(string);
+	string Transaction(double) final;
 
 } HELOC;
