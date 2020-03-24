8c45c8a6e4b33ba39e36f6634e92a14e123b9eb9
agallegos-13 <61513494+agallegos-13@users.noreply.github.com>
2020-03-24 13:12:49 -0600

    removed all couts and added defaults
    no overdraft protection/looping

--- HELOC.cpp
+++ HELOC.cpp
@@ -13,93 +13,55 @@
 #include "HELOC.h"
 #include <string>
 
-/*HELOC::HELOC() {
-    double houseval = 0, mortgage = 0;
-    cout << "What is the current value of your property? ";
-    cin >> houseval;
-    cout << "What is your current mortgage? ";
-    cin >> mortgage;
-    months = 0;
+HELOC::HELOC() {
+    double houseval = 125000, mortgage = 75000;
+    months = 80;
     equity = houseval - mortgage;
     loan = equity - (equity * .2);
-}*/
-
-/*HELOC::~HELOC() {
-
-}*/
+}
 
 void HELOC::setLoan(double SetLoan) {
     loan = SetLoan;
 }
 
+string HELOC::fixDouble(string doubl) {
+	int index = static_cast<int>(doubl.length() - 1);
+	for (int i = 0; i < static_cast<int>(doubl.length()); i++) {
+		if (doubl[static_cast<int>(doubl.length() - 1) - i] == '0') {
+			index--;
+		}
+		else {
+			break;
+		}
+	}
+	doubl = doubl.substr(0, index + 1);
+	return doubl;
+}
+
 string HELOC::Transaction(double amt) {
-    char user, input, temp, /*userChoice, */choice;
-    cout << "Do you have an existing HELOC?\n (1) Yes (2) No\nInput 1 or 2 then [Enter]\n";
-        cin >> user;
-    if (user = 1) choice = 1;
-    if (user = 2) choice = 2;
-    //if (user != 1 || user != 2) choice = 3;
-    else {choice = 3;}
+    char user, input, temp;
+    double owed = loan;
+    if (amt < 0) choice = 1;
+    if (amt > 0) choice = 2;
     switch (choice) {
     case 1:
-        //User Has existing HELOC
-        cout << "What would you like to view from your HELOC?\n(1) Remaining Payment: \n"
-            << "(2) Current interest: \n(3) Home Equity:" << endl;
-        cin >> user;
-
-        if (user = 1) {
-            cout << "The Remaining payment in your HELOC is "/* << */ << endl;
-            
-        }
-
-        if (user = 2) {
-            cout << "The current interest on your HELOC is "/* << */ << endl;
-
-        }
-
-        //May not be needed
-        if (user = 3) {
-            cout << "The current value of your home equity is? "/* << */ << endl;
-
-        }
+            owed += amt;
+            loan -= amt;
             break;
 
     case 2:
-        //User is creating a HELOC(maybe?)
-        cout << "Your maximum loan is worth: $" << loan << endl;
-        cout << "Would you like to activate your HELOC? (y/n)" << endl; //assuming user is eligible
-        cin >> user;
-        if ((user == 'Y') || (user == 'y')) {
-            cout << "For the first ten years of your loan, you will owe " << intr1 << "%." << endl;
-            cout << "After the ten years of the drawing period is up, you will pay back what has been spent plus an interest of " << intr2 << "%." << endl;
-            cout << "Would you like to draw from your loan now? (y/n): ";
-            cin >> input;
-            if ((input == 'Y') || (input == 'y')) {
-                if (amt > loan) {
-                    cout << "Your previously selected value was too large, please enter a new value." << endl;
-                    cin >> amt;
-                    loan -= amt;
-                }
-                else {
-                    cout << "Previously you wanted to draw $" << amt << ".\nIs this still accurate? (y/n): ";
-                    cin >> temp;
-                    if ((temp == 'y') || (temp == 'Y')) loan -= amt;
-                    else if ((temp == 'n') || (temp == 'N')) {
-                        cout << "Please enter a new amount: $";
-                        cin >> amt;
-                        amt -= loan;
-                    }
-                }
+            if (months => 120){
+                owed += (loan * intr2);
             }
-            cout << "You have " << loan << " remaining." << endl;
-            amt* intr1;
-            cout << "Currently you will owe $" << amt << "." << endl;
-        }
+            else if (months < 120){
+                owed += (loan * intr1);
+            }
             break;
         default:
             break;
 
     }
     string trans = to_string(loan);
+    fixDouble(trans);
     return trans;
 }
